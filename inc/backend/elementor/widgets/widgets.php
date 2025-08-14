94c5a06dba64bd48c97ceecc99ef8354941abceb
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2025-08-14 19:31:49 +0500

    sdfsdfsdf

--- inc/backend/elementor/widgets/widgets.php
+++ inc/backend/elementor/widgets/widgets.php
@@ -47,3 +47,4 @@ locate_template('/inc/backend/elementor/widgets/new_widget.php', true, true);
 locate_template('/inc/backend/elementor/widgets/new_widget2.php', true, true);
 locate_template('/inc/backend/elementor/widgets/new_widget3.php', true, true);
 locate_template('/inc/backend/elementor/widgets/new_widget4.php', true, true);
+locate_template('/inc/backend/elementor/widgets/xp-list-showcase.php', true, true);
