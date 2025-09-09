a793e0b9b894a94628c00a4638dce8b222cad0ed
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2025-09-09 16:08:35 +0500

    sdfsdf

--- inc/backend/color.php
+++ inc/backend/color.php
@@ -8,7 +8,7 @@ if(!function_exists('skinetic_color_scheme')){
 
 	  	// Get theme options with fallbacks
 		$primary_color      = skinetic_get_option('primary_color', '#b58c67');
-		$secondary_color    = skinetic_get_option('secondary_color', '#e9dfde');
+		$secondary_color    = skinetic_get_option('secondary_color', '#cccccc');
 		$text_color         = skinetic_get_option('text_color', '#1d1312');
 		$accent_color       = skinetic_get_option('accent_color', '#8f605c');
 		$bg_color           = skinetic_get_option('bg_color', '#f4efef');
