c903c408b9ac388436b5346eed42d34b74cf4f0a
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2025-09-09 16:45:05 +0500

    sdfsdfdsf

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -46,19 +46,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 		do_action( 'woocommerce_before_shop_loop_item_title' );
 		?>
 		</a>
-		<div class="wrapper-add-to-cart">
-			<div class="add-to-cart-inner">
-				<?php
-				/**
-				 * Hook: woocommerce_after_shop_loop_item.
-				 *
-				 * @hooked woocommerce_template_loop_product_link_close - 5
-				 * @hooked woocommerce_template_loop_add_to_cart - 10
-				 */
-				do_action( 'woocommerce_after_shop_loop_item' );
-				?>			
-			</div>
-		</div>
+		
 	</div>
 
 	<?php
@@ -78,4 +66,17 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	do_action( 'woocommerce_after_shop_loop_item_title' );
 	
 	?>
+	<div class="wrapper-add-to-cart">
+			<div class="add-to-cart-inner">
+				<?php
+				/**
+				 * Hook: woocommerce_after_shop_loop_item.
+				 *
+				 * @hooked woocommerce_template_loop_product_link_close - 5
+				 * @hooked woocommerce_template_loop_add_to_cart - 10
+				 */
+				do_action( 'woocommerce_after_shop_loop_item' );
+				?>			
+			</div>
+		</div>
 </li>
