594f4f111a1e888b93138c214b6a7205289272ea
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2025-09-23 17:32:33 +0500

    sdfsdfsdf

--- inc/frontend/preloader.php
+++ inc/frontend/preloader.php
@@ -87,7 +87,7 @@ function preloader_customize_settings() {
             'type'     => 'color',
             'label'    => esc_html__( 'Percent Text Color', 'skinetic' ),
             'section'  => 'preload_section',
-            'default'  => '#0a0f2b',
+            'default'  => 'var(--white-color)',
             'priority' => 14,
             'active_callback' => array(
                 array(
@@ -101,7 +101,7 @@ function preloader_customize_settings() {
             'type'     => 'color',
             'label'    => esc_html__( 'Background Color', 'skinetic' ),
             'section'  => 'preload_section',
-            'default'  => '#fff',
+            'default'  => 'var(--accent-color)',
             'priority' => 15,
             'active_callback' => array(
                 array(
