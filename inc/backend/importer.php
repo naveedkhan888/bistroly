dba1983c1f7008fe29d682696a55fdfe7a468396
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2025-10-06 16:09:05 +0500

    sdfsdf

--- inc/backend/importer.php
+++ inc/backend/importer.php
@@ -37,7 +37,7 @@ function bistroly_importer() {
 			'name'       => 'Home 2',
 			'preview'    => get_template_directory_uri().'/inc/backend/data/maintheme/home2.jpg',
 			'content'    => get_template_directory_uri().'/inc/backend/data/maintheme/demo-content.xml',
-			'customizer' => get_template_directory_uri().'/inc/backend/data/maintheme/customizer.dat',
+			'customizer' => get_template_directory_uri().'/inc/backend/data/maintheme/customizer-coffee.dat',
 			'widgets'    => get_template_directory_uri().'/inc/backend/data/maintheme/widgets.wie',
 			//'sliders'    => '/inc/backend/data/main/sliders.zip',
 			'pages'      => array(
@@ -56,11 +56,49 @@ function bistroly_importer() {
 			'name'       => 'Home 3',
 			'preview'    => get_template_directory_uri().'/inc/backend/data/maintheme/home3.jpg',
 			'content'    => get_template_directory_uri().'/inc/backend/data/maintheme/demo-content.xml',
-			'customizer' => get_template_directory_uri().'/inc/backend/data/maintheme/customizer.dat',
+			'customizer' => get_template_directory_uri().'/inc/backend/data/maintheme/customizer-icecream.dat',
 			'widgets'    => get_template_directory_uri().'/inc/backend/data/maintheme/widgets.wie',
 			//'sliders'    => '/inc/backend/data/main/sliders.zip',
 			'pages'      => array(
 				'front_page' => 'Home 3',
+				'blog'       => 'Blog',
+				'shop'       => 'Shop',
+				'cart'       => 'Cart',
+				'checkout'   => 'Checkout',
+				'my_account' => 'My Account',
+			),
+			'menus'      => array(
+				'primary'   => 'main-menu',
+			)
+		),
+		array(
+			'name'       => 'Home 4',
+			'preview'    => get_template_directory_uri().'/inc/backend/data/maintheme/home4.jpg',
+			'content'    => get_template_directory_uri().'/inc/backend/data/maintheme/demo-content.xml',
+			'customizer' => get_template_directory_uri().'/inc/backend/data/maintheme/customizer-pizza.dat',
+			'widgets'    => get_template_directory_uri().'/inc/backend/data/maintheme/widgets.wie',
+			//'sliders'    => '/inc/backend/data/main/sliders.zip',
+			'pages'      => array(
+				'front_page' => 'Home 4',
+				'blog'       => 'Blog',
+				'shop'       => 'Shop',
+				'cart'       => 'Cart',
+				'checkout'   => 'Checkout',
+				'my_account' => 'My Account',
+			),
+			'menus'      => array(
+				'primary'   => 'main-menu',
+			)
+		),
+		array(
+			'name'       => 'Home 5',
+			'preview'    => get_template_directory_uri().'/inc/backend/data/maintheme/home5.jpg',
+			'content'    => get_template_directory_uri().'/inc/backend/data/maintheme/demo-content.xml',
+			'customizer' => get_template_directory_uri().'/inc/backend/data/maintheme/customizer-juice.dat',
+			'widgets'    => get_template_directory_uri().'/inc/backend/data/maintheme/widgets.wie',
+			//'sliders'    => '/inc/backend/data/main/sliders.zip',
+			'pages'      => array(
+				'front_page' => 'Home 5',
 				'blog'       => 'Blog',
 				'shop'       => 'Shop',
 				'cart'       => 'Cart',
